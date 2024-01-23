cc51317acddc1a48e525f8162908231ef7bd5ab1
Uezili <weslmaciel@gmail.com>
2024-01-23 16:41:38 -0300

    Adjust dependency.

--- src/test/java/cucumber/steps/StoreProductsSteps.java
+++ src/test/java/cucumber/steps/StoreProductsSteps.java
@@ -1,13 +1,9 @@
 package cucumber.steps;
 
-import io.cucumber.java.en.Then;
+import io.cucumber.java.en.Given;
 
-import io.cucumber.spring.CucumberContextConfiguration;
-
-
-@CucumberContextConfiguration
 public class StoreProductsSteps {
-    @Then("All product should be returned successfully")
-    public void allProductShouldBeReturnedSuccessfully() {
+    @Given("i have more than {int} products in my store")
+    public void iHaveMoreThanProductsInMyStore(int arg0) {
     }
 }
