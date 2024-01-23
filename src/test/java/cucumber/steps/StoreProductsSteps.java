61f0c2ba71468941081c1f7ce52371d35b321299
Uezili <weslmaciel@gmail.com>
2024-01-23 13:49:07 -0300

    New dependency.

---
+++ src/test/java/cucumber/steps/StoreProductsSteps.java
@@ -0,0 +1,9 @@
+package cucumber.steps;
+
+import io.cucumber.java.en.Then;
+
+public class StoreProductsSteps {
+    @Then("All product should be returned successfully")
+    public void allProductShouldBeReturnedSuccessfully() {
+    }
+}
